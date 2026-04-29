800e1e784c54f586b2b75926061b894ac29dbd80
Githyr2864 <githubven@gmail.com>
2026-04-29 23:41:27 +0300

    Delete is-lab-app-main directory

--- is-lab-app-main/Models/Note.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IsLabApp.Models;
-
-public class Note
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Text { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; }
-}
